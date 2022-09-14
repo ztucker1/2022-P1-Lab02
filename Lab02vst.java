3d9de9536ac2aa07eca993018e8d29c59a8abd79
Mr. Kiedes <akiedes@vtsd.com>
2022-09-14 07:49:43 -0400

    Code Update Upload

--- Lab02vst.java
+++ Lab02vst.java
@@ -1,1 +1,14 @@
-  
+/*
+    Welcome to your first lab!  All work should be completed inside of this file.
+    Please make sure that as you work on your lab you save, commit, and push everyday.
+
+    As always, if you have any questions reach out and email me!
+
+*/
+
+  public class Lab02vst {
+
+    public static void main(String args[]){
+
+    }
+  }
