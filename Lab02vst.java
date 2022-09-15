58ae923a02e0d27bc6392b53b090290efa414790
13433 <natalie.tucker@vtsd.com>
2022-09-15 08:06:33 -0400

    done :)

--- Lab02vst.java
+++ Lab02vst.java
@@ -9,6 +9,17 @@
   public class Lab02vst {
 
     public static void main(String args[]){
+      System.out.println("Lab 02, 100 Point version");
+      System.out.println();
+      System.out.println();
+      System.out.print("Measure today's knowledge");
+      System.out.println(" by yesterday's confusion.");
+      System.out.println();
+      System.out.println("# ##### #####   ##### #####  #  #   # ##### #####");
+      System.out.println("# #   # #   #   #   # #   #  #  ##  #   #   #");
+      System.out.println("# #   # #   #   ##### #   #  #  # # #   #   #####");
+      System.out.println("# #   # #   #   #     #   #  #  #  ##   #       #");
+      System.out.println("# ##### #####   #     #####  #  #   #   #   #####");
 
     }
   }
